e4050c9ba78e013483cf126c1f28e07cfaebae87
Simrantajk <86843740+Simrantajk@users.noreply.github.com>
2023-10-26 13:37:35 +0530

    Add files via upload

---
+++ task1.java
@@ -0,0 +1,77 @@
+package package1;
+import java.util.Random;
+import java.util.*;
+
+public class task1 {
+	static int trial = 5;
+	static int score =0;
+	static boolean flag = false;
+	static int ratio=1;
+	static int guess=0;
+	static int rn;
+	static int i;
+	
+	public static void check()
+	{
+		System.out.println("Guess the random number");
+		Scanner sc = new Scanner(System.in);
+		guess = sc.nextInt();
+		if(guess == rn)
+		{
+			System.out.println("Correct guess");
+			score = score + 1;
+			flag = true;
+			return;
+		}
+		ratio =Math.abs( rn - guess);
+		if(ratio < 10)
+		System.out.println("Yayyy, 90% close to the guess");
+		else if(ratio < 50)
+			System.out.println("High guess, You are 50-90% close to the correct guess");
+		else
+			System.out.println("low guess");
+		if(i==trial)
+		{
+		System.out.println("\n\n\nYOu ran out of chnaces \n GOOd try and the random number is ");
+		System.out.println(rn);
+		again();
+		}
+	}
+	public static void again()
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("\n\n\nDo you wanna restart again? 1 for Yes and 0 for No");
+		int retry = sc.nextInt();
+		if(retry == 1)
+		{
+			i =1;
+			start();
+		}
+		else
+			System.out.println("Hope you enjoyed olaying it ");
+			System.exit(0);
+		
+	}
+	public static void start()
+	{
+		for(i =1; i<=trial; i++) {
+			if(flag == true) {
+				System.out.println("Thank you for Playing");
+				again();
+			}
+		check();
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random rand = new Random();
+		rn = rand.nextInt(0,100);
+		Scanner sc = new Scanner(System.in);
+		start();
+		System.out.println("Score is "+ score);
+			
+		
+
+	}
+
+}
---
+++ task2.java
@@ -0,0 +1,63 @@
+package package1;
+import java.util.*;
+
+public class task2 {
+	static int m1;
+	static int m2;
+	static int m3;
+	static int m4;
+	static int m5;
+	static int m6;
+	static int total;
+	static int avgper;
+	static char grade;
+	public static void getdata()
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the marks of all Six subjects:");
+		m1 = sc.nextInt();
+		m2 = sc.nextInt();
+		m3 = sc.nextInt();
+		m4 = sc.nextInt();
+		m5 = sc.nextInt();
+		m6 = sc.nextInt();	
+	}
+	public static void calculate()
+	{
+		total = m1 + m2 + m3 + m4 + m5 + m6;
+		avgper = total/6;
+		if(avgper>=90)
+		{
+			grade = 'A';
+		}
+		else if(avgper>= 80)
+		{
+			grade = 'B';
+		}
+		else if(avgper>=70)
+		{
+			grade = 'C';
+		}
+		else if(avgper>=60)
+		{
+			grade = 'D';
+		}
+		else
+		{
+			grade = 'F';
+		}
+	}
+	public static void display()
+	{
+		System.out.println("\n\nTotal Marks = "+ total);
+		System.out.println("Average Percentage = "+ avgper);
+		System.out.println("Grade "+ grade );
+		
+	}
+	public static void main(String[] args) {
+		getdata();
+		calculate();
+		display();
+	}
+
+}
